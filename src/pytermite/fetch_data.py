07bec609d32086cd5e03828bb025d2eb168ef69a
Jonas Laptop <jonasrostan@gmail.com>
2026-06-18 16:38:19 +0200

    fix(api): file processed on camera check loop

--- src/pytermite/fetch_data.py
+++ src/pytermite/fetch_data.py
@@ -2,6 +2,7 @@ import requests
 from pathlib import Path
 import json
 import time
+import os
 
 from pytermite.connection import (
     WiredConnection
@@ -9,7 +10,8 @@ from pytermite.connection import (
 
 def fetch_recorded( serials: dict[str, str] | set[str] | None = None, 
                     save_path: str|None = None, 
-                    logger = None
+                    logger = None,
+                    allowed_retries = 10
     ):
     logger_available = logger is not None
     if (serials is None or len(serials) < 1):
@@ -23,6 +25,7 @@ def fetch_recorded( serials: dict[str, str] | set[str] | None = None,
         save_path = Path(save_path)
 
     for serial_nr in serials:
+        save_path = save_path / serial_nr[-4:]
         ip = f"172.2{serial_nr[-3]}.1{serial_nr[-2:]}.51:8080"
 
         url_last = f"http://{ip}/gopro/media/last_captured"
@@ -30,21 +33,27 @@ def fetch_recorded( serials: dict[str, str] | set[str] | None = None,
 
         response_data = json.loads(response_last.text)
         if response_last.status_code == 200:
-            url_info = f"http://{ip}/gopro/media/info"
-            querystring = {"path":f"{response_data["folder"]}/{response_data["file"]}"}
+            url_info = f"http://{ip}/gopro/media/info?path={response_data["folder"]}/{response_data["file"]}"
 
             counter = 0
-            while counter < 10:
-                response = json.loads(requests.request("GET", url, params=querystring).text)
-                if response["tr"] == 1:
+            while counter < allowed_retries:
+                response = requests.request("GET", url_info)
+                if response_last.status_code == 200:
                     break
                 counter += 1
                 time.sleep(1)
+            if counter >= allowed_retries:
+                logger.warning(f"Timeout: Data of {serial_nr[-4:]} could not be fetched. Filename: {response_data["file"]}")
+                continue
 
             url = f"http://{ip}/videos/DCIM/{response_data["folder"]}/{response_data["file"]}"
             response = requests.request("GET", url)
-            with open(save_path / response_data["file"], "wb") as f:
-                f.write(response.content)
+            if response.status_code == 200:
+                os.make_dir(save_path, exists_ok=True)
+                with open(save_path / response_data["file"], "wb") as f:
+                    f.write(response.content)
+            else:
+                logger.warning(f"Unknown: Data of {serial_nr[-4:]} could not be fetched. Filename: {response_data["file"]}")
 
         elif logger_available:
             # TODO: How to map on name of cam? Might be more readable
